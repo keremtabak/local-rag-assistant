94ad4b0c3b96be44830087cc8bc074ce36f2395c
Muhammet Kerem Tabak <20247170091@ogr.oku.edu.tr>
2026-08-15 12:46:16 +0300

    Added PDF reader support and PDF test document

--- ingest.py
+++ ingest.py
@@ -1,12 +1,16 @@
 from docx import Document
+from pypdf import PdfReader
 
 def read_txt_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
+def read_pdf_file(file_path):
+    reader = PdfReader(file_path)
+    return "\n".join([page.extract_text() for page in reader.pages])
 def read_docx_file(file_path):
     doc = Document(file_path)
     return "\n".join([paragraph.text for paragraph in doc.paragraphs])
 
 if __name__ == "__main__":
-    content = read_docx_file("data/test.docx")
+    content = read_pdf_file("data/test.pdf.pdf")
     print(content)
