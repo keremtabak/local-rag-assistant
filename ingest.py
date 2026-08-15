8a1e193f8faa2c6110a5fa0fdd120f8af3387c3e
Muhammet Kerem Tabak <20247170091@ogr.oku.edu.tr>
2026-08-15 13:02:08 +0300

    Added automatic file type detection

--- ingest.py
+++ ingest.py
@@ -10,7 +10,15 @@ def read_pdf_file(file_path):
 def read_docx_file(file_path):
     doc = Document(file_path)
     return "\n".join([paragraph.text for paragraph in doc.paragraphs])
+def read_file(file_path):
+    if file_path.endswith(".txt"):
+        return read_txt_file(file_path)
 
+    if file_path.endswith(".docx"):
+        return read_docx_file(file_path)
+
+    if file_path.endswith(".pdf") or file_path.endswith(".pdf.pdf"):
+        return read_pdf_file(file_path)
 if __name__ == "__main__":
-    content = read_pdf_file("data/test.pdf.pdf")
+    content = read_file("data/test.docx")
     print(content)
