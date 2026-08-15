b06b495613ff0baabb9069b35679916879214623
Muhammet Kerem Tabak <20247170091@ogr.oku.edu.tr>
2026-08-15 16:17:01 +0300

    Added embeddings from SQLite documents

--- rag.py
+++ rag.py
@@ -1,12 +1,18 @@
 from sentence_transformers import SentenceTransformer
+from database import get_documents
 
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 print("Model loaded successfully")
 
-embedding = model.encode(
-    "Bu benim ilk embedding testimdir."
-)
+documents = get_documents()
+
+first_chunk = documents[0][2]
+
+embedding = model.encode(first_chunk)
 
 print(len(embedding))
+print(embedding[:5])
+
+print(first_chunk)
